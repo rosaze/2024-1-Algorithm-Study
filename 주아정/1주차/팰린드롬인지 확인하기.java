c33461f88cb32cd8daaad0241a1ef45a7f0bf080
아정 <80907516+ajung7038@users.noreply.github.com>
2024-05-07 01:51:01 +0900

    Move: 1주차 폴더 이동 및 문제 이름 변경

--- 주아정/1주차/팰린드롬인지 확인하기.java
+++ 주아정/1주차/팰린드롬인지 확인하기.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+// BOJ_10988
 public class Main {
 
     public static void main(String[] args) throws IOException {
@@ -11,7 +12,7 @@ public class Main {
         System.out.println(isPalindrome(word, 0, word.length()-1));
     }
 
-    public static int isPalindrome(String word, int start, int end) {
+    public static int isPalindrome(String word, int start, int end) { // 재귀적으로 구현
         if (start >= end) {
             return 1;
         } else if (word.charAt(start) == word.charAt(end))
